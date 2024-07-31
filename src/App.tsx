2f936df291204a2bbfefab6b5e34410a4703fd42
Kelias1 <kelsito24@gmail.com>
2024-07-31 19:11:42 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Stars from './components/Stars/Stars';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Рейтинг фильма</h1>
+			<Stars count={3} />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Stars/Stars.tsx
@@ -0,0 +1,23 @@
+import { Star } from '../Star/Star';
+
+interface StarsProps {
+  count?: number;
+}
+
+const Stars: React.FC<StarsProps> = ({ count = 0 }) => {
+  if (typeof count !== 'number' || count < 1 || count > 5) {
+    return null;
+  }
+
+  const starsArray = Array(count).fill(null);
+
+  return (
+    <ul className="card-body-stars u-clearfix">
+      {starsArray.map((_, index) => (
+        <Star key={index} />
+      ))}
+    </ul>
+  );
+};
+
+export default Stars;
